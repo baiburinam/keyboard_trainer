8acdd1b446e88991e786757a82edaa30347a75d4
baiburinam <141995774+baiburinam@users.noreply.github.com>
2023-11-23 21:10:40 +0300

    classes.py
    class Mistake, class Results, class TimeSpeed.

---
+++ classes.py
@@ -0,0 +1,24 @@
+class Mistake:
+    all_mst = 0
+    all_chr = 0
+
+    def __init__(self):
+        self.count = 0
+
+
+class Results:
+    task = {}
+
+    def __init__(self):
+        self.key = 1
+        self.speed = 0
+        self.mistake = 0
+        self.acc = 0
+
+
+class TimeSpeed:
+    all_time = 0
+
+    def __init__(self):
+        self.start = 0
+        self.end = 0
